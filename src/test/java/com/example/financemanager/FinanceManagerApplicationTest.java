9c10ff2e227e1e3ef610aeb440d4f1fc82c2ffbc
Eric Hannafin <erichannafin1990@gmail.com>
2024-02-15 23:02:49 +0000

    Addressing code smells

--- src/test/java/com/example/financemanager/FinanceManagerApplicationTest.java
+++ src/test/java/com/example/financemanager/FinanceManagerApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FinanceManagerApplicationTests {
+class FinanceManagerApplicationTest {
 
     @Test
     void contextLoads() {
