a3482be748d6e5ef3698295724dced6c49cea5cd
Eric Hannafin <134812781+Eric-Hannafin@users.noreply.github.com>
2024-08-05 20:47:58 +0100

    Update RefreshRequest.java
    Updating RefreshToken class

--- src/main/java/com/example/financemanager/auth/model/RefreshRequest.java
+++ src/main/java/com/example/financemanager/auth/model/RefreshRequest.java
@@ -1,4 +1,13 @@
 package com.example.financemanager.auth.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class RefreshRequest {
+
+    private String refreshToken;
 }
